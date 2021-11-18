f27ad44f2af11f68dd4a41fdddf4bb68c6fb94c4
Ilkin <ilkin.isgenderov91@gmail.com>
2021-11-18 18:24:42 +0400

    index.php deywiklik edildi

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once("vendor/fabpot/goutte/Goutte/Client.php");
 
 use Goutte\Client;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\DomCrawler\Crawler;
 ?>
 <style>
     .input_text{
@@ -42,60 +41,79 @@ use Symfony\Component\DomCrawler\Crawler;
 <?
 
 if (isset($_POST["txtInput"]) && $_POST["txtInput"] != "") {
-    $term = $_POST["wv"][0];
-    echo $term;
+    $term = $_POST["txtInput"];
+
     ini_set('max_execution_time', '500');
     $client = new Client(HttpClient::create(['timeout' => 500]));
+
+    //search page define
     $crawler1 = $client->request('GET', 'https://search.ipaustralia.gov.au/trademarks/search/quick/result?q=' . $term);
-    $count = $crawler1->filter('h2')->each(function ($node) {
-        "<br>Total result is: ".$node->text() ?? null;
-        return ceil(($node->text() ?? null) /100);
-    });
+    //Get total result from parser html.
+    $count = $crawler1->filter('h2')->first();
 
-    for ($i = 0; $i < $count[0]; $i++) {
-        $crawler2 = $client->request('GET', 'https://search.ipaustralia.gov.au/trademarks/search/quick/result?q='.$term.'&p='.$i);
-
-        $result = $crawler2->filter('.js-mark-record')->each(function ($node) {
-
-            $number = $node->filter('.qa-tm-number')->each(function ($a) {
-                return  $a->text();
-            });
-
-            $name = $node->filter('.trademark')->each(function ($a) {
-                return  $a->text();
-            });
-
-            $class = $node->filter('.classes')->each(function ($a) {
-                return  $a->text();
-            });
-
-            $status = $node->filter('.status')->each(function ($a) {
-                return  $a->text();
-            });
-
-            $img = $node->filter('img')->each(function ($a) {
-                return  $a->attr('src');
-            });
-
-            $details_page_url =  $node->filter('a')->each(function ($a) {
-                return  $a->attr('href');
-            });
-
-
-            $items = [
-                'number' =>  $number[0],
-                'logo_url' => $img[0] ?? null,
-                'name' => $name[0],
-                'classes' => $class[0],
-                'status' => $status[0],
-                'details_page_url' => "https://search.ipaustralia.gov.au"."".$details_page_url[0] ?? null
-            ];
-
-            return $items;
-        });
+    if($count->count() > 0){
+        $totalres =$count->text();
     }
-    echo '<pre>';
-    print_r($result ?? null);
+
+    //Each element we get result
+    //print_r($count);
+    if($totalres > 0){
+        echo 'Total result is: <b>'.$totalres.'</b>';
+        $sum = 0;
+        $items = array();
+        for ($i = 0; $i < $totalres; $i++) {
+            $sum +=$i;
+            $crawler2 = $client->request('GET', 'https://search.ipaustralia.gov.au/trademarks/search/quick/result?q='.$term.'&p='.$i);
+            $result = $crawler2->filter('.js-mark-record')->each(function ($node){
+
+                $number = $node->filter('.qa-tm-number')->first();
+                if($number->count() >0){
+                    $number = $number->text();
+                }
+
+                $name = $node->filter('.trademark')->first();
+                if($name->count() >0){
+                    $name = $name->text();
+                }
+
+                $class = $node->filter('.classes')->first();
+                if($class->count() >0){
+                    $class = $class->text();
+                }
+
+                $status = $node->filter('.status')->first();
+                if($status->count() >0){
+                    $status = $status->text();
+                }
+
+
+
+                $details_page_url = $node->filter('a')->first();
+                if($details_page_url->count() >0){
+                    $details_page_url = $details_page_url->text();
+                }
+
+
+                $items = array(
+                    'number' =>  $number,
+                    'logo_url' => '$img ?? null',
+                    'name' => $name,
+                    'classes' => $class,
+                    'status' => $status,
+                    'details_page_url' => "https://search.ipaustralia.gov.au"."".$details_page_url ?? null
+                );
+
+            });
+
+        }
+        echo '<pre>';
+        var_dump($items);
+
+
+    }else{
+        echo "Empty list";
+        exit();
+    }
 
 }else{
     echo "Please fill out this field";
